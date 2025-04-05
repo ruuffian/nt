cf49c4578968fc2eaad0adcfd09e0e70b18d2655
ruuffian <ruuffian@gmail.com>
2025-04-04 21:37:40 -0400

    Refactored delete function to work with dynamic hash table. Time to tighten up some typing and start working on external chaining :D

--- c/hash_table/hash_table.c
+++ c/hash_table/hash_table.c
@@ -1,6 +1,5 @@
 #include "hash_table.h"
 
-ack *hash_t[TABLE_SIZE];
 int main() {
   hash_table *table = hash_table_create(15);
   if (table == NULL) printf("Failed to create hash table...");
@@ -10,12 +9,13 @@ int main() {
   ack four_0 = { .pair={.m=4, .n=0}, .value=13};
   ack ten_3 = { .pair={.m=10, .n=3}, .value=0};
   printf("Inserting (0,0), (3,10), (4,0)...\n");
-  hash_table_insert(table, &zero_0);
-  hash_table_insert(table, &three_10);
-  hash_table_insert(table, &four_0);
+  hash_table_insert(table, zero_0.pair, &zero_0);
+  hash_table_insert(table, three_10.pair, &three_10);
+  hash_table_insert(table, four_0.pair, &four_0);
   print_hash_table(table);
+  ack *tmp;
   // Does exist
-  ack *tmp = hash_table_lookup(table, three_10.pair);
+  tmp = hash_table_lookup(table, three_10.pair);
   printf("Checking (3, 10):\t");
   if (tmp == NULL) {
     printf("Could not find (3,10)...something is wrong.\n");
@@ -30,23 +30,21 @@ int main() {
   } else {
     printf("Found (10, 3)...something went wrong.\n");
   }
-  /*
-  printf("Deleting (3, 10)...\t");
-  tmp = hash_table_delete(&three_10);
+  printf("Deleting (3, 10):\t");
+  tmp = hash_table_delete(table, three_10.pair);
   if (tmp == NULL) {
-    printf("Object not found...\n");
+    printf("Could not find (3,10)...something bad happened.\n");
   } else {
-    printf("Deleted (%d, %d)\n", tmp->pair.m, tmp->pair.n);
+    printf("Deleted (3, 10)!\n");
   }
-  printf("Deleting (3, 10)...\t");
-  tmp = hash_table_delete(&three_10);
+  printf("Deleting (3, 10):\t");
+  tmp = hash_table_delete(table, three_10.pair);
   if (tmp == NULL) {
-    printf("Object not found...\n");
+    printf("Could not find (3, 10)!\n");
   } else {
-    printf("Deleted (%d, %d)\n", tmp->pair.m, tmp->pair.n);
+    printf("Deleted (3, 10)...something bad happened.\n");
   }
-  print_hash_table();
-  */
+  print_hash_table(table);
   hash_table_destroy(table);
   return 0;
 }
@@ -58,7 +56,6 @@ hash_table *hash_table_create(size_t size) {
   ht->size = size;
   ht->table = (ack **)calloc(size, sizeof(ack *));
   if (ht->table == NULL) return NULL;
-  //hash_table->table[size - 1] = NULL;
   return ht;
 }
 
@@ -67,45 +64,33 @@ void hash_table_destroy(hash_table *ht) {
   free(ht);
 }
 
-bool hash_table_insert(hash_table *hash_table, ack *p) {
-  if (hash_table == NULL || p == NULL) return false;
-  int index = hash(p->pair, hash_table->size);
-  if (hash_table->table[index] != NULL) {
+bool hash_table_insert(hash_table *ht, pair key, ack *value) {
+  if (ht == NULL || value == NULL) return false;
+  int index = _hash(key, ht->size);
+  if (ht->table[index] != NULL) {
     return false;
   }
-  hash_table->table[index] = p;
+  ht->table[index] = value;
   return true;
 }
-/*
-ack *hash_table_delete(ack *p) {
-  if (p == NULL) return NULL;
-  int index = hash(p->pair);
-  if (hash_t[index] != NULL &&
-    pair_cmp(hash_t[index]->pair, p->pair) == 0) {
-    ack *tmp = hash_t[index];
-    hash_t[index] = NULL;
+
+ack *hash_table_delete(hash_table *ht, pair pair) {
+  if (ht == NULL) return NULL;
+  int index = _hash(pair, ht->size);
+  if (ht->table[index] != NULL &&
+    _pair_cmp(ht->table[index]->pair, pair) == 0) {
+    ack *tmp = ht->table[index];
+    ht->table[index] = NULL;
     return tmp;
   }
-  return NULL;
+  return NULL; 
 }
-*/
 
 ack *hash_table_lookup(hash_table * ht, pair pair) {
-  int index = hash(pair, ht->size);
+  int index = _hash(pair, ht->size);
   if (ht->table[index] != NULL &&
-    pair_cmp(ht->table[index]->pair, pair) == 0) return ht->table[index];
+    _pair_cmp(ht->table[index]->pair, pair) == 0) return ht->table[index];
   return NULL;
-}
-
-unsigned int hash(pair pair, size_t size) {
-  int m = pair.m;
-  int n = pair.n;
-  return ((m*m + m + 2*m*n + 3*n + n*n) / 2) % size;
-}
-
-int pair_cmp(pair p1, pair p2) {
-  if (p1.m == p2.m && p1.n == p2.n) return 0;
-  return 1;
 }
 
 void print_hash_table(hash_table *ht) {
@@ -119,4 +104,15 @@ void print_hash_table(hash_table *ht) {
     }
   }
   printf("\n");
+}
+
+static unsigned int _hash(pair pair, size_t size) {
+  int m = pair.m;
+  int n = pair.n;
+  return ((m*m + m + 2*m*n + 3*n + n*n) / 2) % size;
+}
+
+static int _pair_cmp(pair p1, pair p2) {
+  if (p1.m == p2.m && p1.n == p2.n) return 0;
+  return 1;
 }
--- c/hash_table/hash_table.h
+++ c/hash_table/hash_table.h
@@ -5,8 +5,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TABLE_SIZE 15
-
 typedef struct {
   int m;
   int n;
@@ -22,12 +20,37 @@ typedef struct {
   size_t size;
 } hash_table;
 
-hash_table *hash_table_create(size_t);
-void hash_table_destroy(hash_table *);
-bool hash_table_insert(hash_table *, ack *);
-ack *hash_table_lookup(hash_table *, pair);
-ack *hash_table_delete(ack *);
-void print_hash_table(hash_table *);
-int pair_cmp(pair p1, pair p2);
-unsigned int hash(pair, size_t); 
+/** 
+ * Initialize a hash_table that can contain 'size' elements.
+ */
+hash_table *hash_table_create(size_t size);
+/**
+ * Frees a hash_table.
+ */
+void hash_table_destroy(hash_table *ht);
+/**
+ * Associate key -> value in ht. Returns true if successful, false otherwise
+ */
+bool hash_table_insert(hash_table *ht, pair key, ack *value);
+ack *hash_table_lookup(hash_table *ht, pair key);
+/**
+ * Remove the value associated with 'key' from the given hash_table. If 
+ * 'key' or 'ht' are null or no value is associated with 'key' in the table,
+ * returns NULL. Otherwise, returns the value associated with 'key'. Remember
+ * to free() this value if it was allocated on the heap.
+ */
+ack *hash_table_delete(hash_table *ht, pair key);
+/**
+ * Prints a hash_table to stdout.
+ */
+void print_hash_table(hash_table *ht);
+/**
+ * Compares 'p1' to 'p2'. Returns 0 if they are identical, 1 otherwise.
+ */
+static int _pair_cmp(pair p1, pair p2);
+/**
+  * Hashes 'key' for insertion in a hash_table with a capacity of 'size'
+  * elements.
+  */
+static unsigned int _hash(pair key, size_t size); 
 #endif
