4ad0cf6902cfa4d4ca7a400e1b5b3f9ead3da6fe
ruuffian <ruuffian@gmail.com>
2025-04-10 17:52:42 -0400

    More linting stuff yayayaayayay!

--- ackermann/c/include/hash_table.h
+++ ackermann/c/include/hash_table.h
@@ -1,19 +1,18 @@
-/* 
-* Author: Liam Mackay
-* Date: April 4th, 2025
-* Description: This is a C-implementation of a hash table, using a tuple of
-* ints as its key and a long as its value. Lookup performance is increidbly 
-* important to this implementation- external chaining and linear probing both
-* had extremely negative impacts on performance. It turns out that ackermann's
-* function's cache lookups are fairly local within the algorithm.
-*/
+/*
+ * Author: Liam Mackay
+ * Date: April 4th, 2025
+ * Description: This is a C-implementation of a hash table, using a tuple of
+ * ints as its key and a long as its value. Lookup performance is increidbly
+ * important to this implementation- external chaining and linear probing both
+ * had extremely negative impacts on performance. It turns out that ackermann's
+ * function's cache lookups are fairly local within the algorithm.
+ */
 #ifndef __HASH_TABLE_H
 #define __HASH_TABLE_H
 
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /* Table key type */
 typedef struct {
@@ -35,7 +34,7 @@ typedef struct {
   size_t size;
 } hash_table;
 
-/** 
+/**
  * Initialize a hash_table that can contain 'size' elements.
  */
 hash_table *hash_table_create(size_t size);
@@ -52,14 +51,14 @@ void hash_table_destroy(hash_table *ht);
  */
 entry *hash_table_insert(hash_table *ht, key key, entry *v);
 
-/** 
- * Lookup a key in a hash_table. If 'key' is found in the table, returns the 
+/**
+ * Lookup a key in a hash_table. If 'key' is found in the table, returns the
  * value associted with it. Otherwise, returns NULL.
  */
 entry *hash_table_lookup(hash_table *ht, key key);
 
 /**
- * Remove the value associated with 'key' from the given hash_table. If 
+ * Remove the value associated with 'key' from the given hash_table. If
  * 'key' or 'ht' are null or no value is associated with 'key' in the table,
  * returns NULL. Otherwise, returns the value associated with 'key'. Remember
  * to free() this value if it was allocated on the heap.
--- ackermann/c/src/hash_table.c
+++ ackermann/c/src/hash_table.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "hash_table.h"
 
 /**
